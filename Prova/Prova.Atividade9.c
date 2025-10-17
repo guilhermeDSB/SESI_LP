085e87d71b577c95cc4da8392d7fbd4d2023eacd
GSB <guilherme.brunetto@protonmail.com>
2025-10-17 10:35:13 -0300

    Create Prova.Atividade9.c

---
+++ Prova/Prova.Atividade9.c
@@ -0,0 +1,15 @@
+#include <stdio.h>
+
+int main() {
+    float num1, num2;
+    printf("Digite o primeiro número: ");
+    scanf("%f", &num1);
+    printf("Digite o segundo número: ");
+    scanf("%f", &num2);
+    if(num1 > num2)
+        printf("O maior número é: %.2f\n", num1);
+    else if(num2 > num1)
+        printf("O maior número é: %.2f\n", num2);
+    else
+        printf("Os dois números são iguais!\n");
+}
